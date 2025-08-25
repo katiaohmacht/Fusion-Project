7c06dad74a1965eba4788049e5accacf9d4e2ed1
katia <kohmacht@astrabeam.com>
2025-08-25 19:45:25 -0400

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 source /opt/ros/humble/setup.bash
 
-workspace = `pwd`
+workspace=`pwd`
+workspace_cam=$workspace/cam
 
-cd $workspace
+cd $workspace/cam
 colcon build 
 source install/setup.bash
 
@@ -13,12 +15,12 @@ source install/setup.bash
 cd $workspace/mmwave_ti_ros/ros2_driver/src/ti_mmwave_rospkg/launch
 ros2 launch 6843AOP_Standard.py &
 
-cd $workspace/src
-sed s/MYWORKSPACE/$workspace/ usb.patch >fix.patch
+cd $workspace/cam/src
+sed s/MYWORKSPACE/$workspace_cam/ usb.patch >fix.patch
 patch -p0 < fix.patch
 
 
-cd $workspace
-ros2 run usb_cam usb_cam_node_exe --ros-args -p pixel_format:=mjpeg2rgb -p camera_info_url:=$workspace/src/camera_info.yaml -p framerate:=15.0 &
+cd $workspace/cam
+ros2 run usb_cam usb_cam_node_exe --ros-args -p pixel_format:=mjpeg2rgb -p camera_info_url:=$workspace/cam/src/camera_info.yaml -p framerate:=15.0 &
 
 ros2 launch radar_camera_fusion overlay.launch.py &
