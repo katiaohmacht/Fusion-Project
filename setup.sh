01afe8fee24b544dfed929293c04de4ee3cd4306
katia <kohmacht@astrabeam.com>
2025-08-27 20:14:48 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -24,13 +24,15 @@ cd $workspace/mmwave_ti_ros/ros2_driver/src/ti_mmwave_rospkg/launch
 ros2 launch 6843AOP_Standard.py &
 
 cd $workspace/cam/src
-sed s/MYWORKSPACE/$workspace_cam/ usb.patch > fix.patch
+sed s%MYWORKSPACE%$workspace_cam% usb.patch > fix.patch
 patch -p0 < fix.patch
 
 cd $workspace/cam
-ros2 run usb_cam usb_cam_node_exe --ros-args -p pixel_format:=mjpeg2rgb -p camera_info_url:=$workspace/cam/src/camera_info.yaml -p framerate:=15.0 &
+
+ros2 run usb_cam usb_cam_node_exe --ros-args -p pixel_format:=mjpeg2rgb -p camera_info_url:=file://$workspace/cam/src/camera_info.yaml -p framerate:=15.0 &
 pid_usb_cam=$!
 
+source install/setup.bash
 ros2 run yolo_overlay yolo_overlay_node &
 pid_yolo=$!
 
